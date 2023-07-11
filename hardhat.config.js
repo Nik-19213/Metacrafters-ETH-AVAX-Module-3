ced93d44ff8e95679fc0f37f6fecb8b210072938
Nitin Kushwah <nitinkushwah19213@gmail.com>
2023-07-11 14:44:51 +0530

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.0",
+  networks: {
+    hardhat: {
+      chainId: 31337 // Local network chain ID
+    }
+  }
+};
